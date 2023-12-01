2ef0800c965df6938bbb767e069553c95fbbed30
reaper2171 <theprodigalson2171@gmail.com>
2023-12-01 19:48:39 +0530

    added getUrlFromHtttp function and test

--- crawl.js
+++ crawl.js
@@ -1,12 +1,30 @@
-function normalizeUrl(urlString){
+const {JSDOM} = require('jsdom');
+
+function getUrlsFromHttp(htmlBody, baseUrl){
+    const urls = [];
+    const dom = new JSDOM(htmlBody);                  //JSDOM - function in jsdom to store the html body a dom object in memory
+    const linkedElements = dom.window.document.querySelectorAll('a');
+    for(const linkedElement of linkedElements){
+        if(linkedElement.href.slice(0,1)==='/'){      //condition for relative urls
+            urls.push(`${baseUrl}${linkedElement.href}`);
+        }
+        else{
+            urls.push(linkedElement.href);           
+        }              
+    }
+    return urls;
+}
+
+function normalizeUrl(urlString){               
     const urlObj = new URL(urlString);
     let url = `${urlObj.hostname}${urlObj.pathname}`;
-    if(url.length>0 && url.slice(-1)==='/'){
+    if(url.length>0 && url.slice(-1)==='/'){         //slice function is used to get string indexes, -1 being 1st from last
         url = url.slice(0,-1);
     }
     return url;
 }
 
-module.exports = {
-    normalizeUrl
+module.exports = {          //code for exporting the functions defined above in other files
+    normalizeUrl,
+    getUrlsFromHttp
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,27 @@
-const {normalizeUrl} = require('./crawl.js');
+const {normalizeUrl, getUrlsFromHttp} = require('./crawl.js');   
 const {test, expect} = require('@jest/globals') 
 
-test('Normalize Url', () => {
+
+//test for checking getUrlsFromHttp function
+test('Get URLs from HTTP', () => {                      //basic syntax for every jest test function
+    const inputHtmlBody =
+    `<html>
+        <body>
+            <a href="http://blog.boot.dev">Boot.dev Relative</a>
+            <a href="/path">Boot.dev Relative</a>
+        </body>
+    </html>`
+    const inputUrl = 'http://blog.boot.dev';
+    const responses = getUrlsFromHttp(inputHtmlBody, inputUrl);
+    const expected = ['http://blog.boot.dev/','http://blog.boot.dev/path'];
+    expect(responses).toEqual(expected);                //test passing condition is defined here
+})
+
+
+//test for checking normalizeUrl function
+test('Normalize Url', () => {                            
     const input = 'https://blog.boot.dev/learn/';
-    const actual = normalizeUrl(input);
+    const actual = normalizeUrl(input);                  
     const expected = 'blog.boot.dev/learn';
-    expect(actual).toEqual(expected);
+    expect(actual).toEqual(expected);                    
 })
