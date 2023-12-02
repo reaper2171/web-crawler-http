2351cbda341e4e472a346b64ea76c81964f57bfe
reaper2171 <theprodigalson2171@gmail.com>
2023-12-02 13:33:34 +0530

    crawl page recursive is working

--- crawl.js
+++ crawl.js
@@ -1,25 +1,49 @@
 const {JSDOM} = require('jsdom');
 
-//function to crawl a single webpage
-async function crawlPage(currentUrl){
+//function to crawl a website where pages object record pages and no. of times they have been crawled
+async function crawlPage(baseUrl, currentUrl, pages){
+    const currentUrlObj = new URL(currentUrl);
+    const baseUrlObj = new URL(baseUrl);
+    if (currentUrlObj.hostname !== baseUrlObj.hostname){   //condition to verify that we are in the same domain name
+        return pages;
+    }
+  
+    const normalizedUrl = normalizeUrl(currentUrl);
+
+    // if we've already visited this page just increase the count and don't repeat the http request
+    if (pages[normalizedUrl] > 0){
+        pages[normalizedUrl]++;
+        return pages;
+    }
+
+    // initialize this page in the map
+     // since it doesn't exist yet
+    pages[normalizedUrl] = 1
+    
     console.log(`Currently crawling ${currentUrl} url`);  //Message for notifying the control flow
+    let htmlBody;
     try{
         const resp = await fetch(currentUrl);             //fetching response object
         
         if(resp.status >399){                   //for handling error status codes
             console.log(`error in fetch status code: ${resp.status} on page ${currentUrl}`);
-            return;
+            return pages;
         }
         //for handling non-html pages
         if(!resp.headers.get("content-type").includes("text/html")){       //used includes and not !== bacuse content-type may have sonme other content-types like charset
             console.log(`Non-html content-type: ${resp.headers.get("content-type")} on page ${currentUrl}`);
-            return;
+            return pages;
         }   
-
-        console.log(await resp.text());         //converts the response object which container info about response(like code,headers,etc) to text
+        htmlBody = await resp.text();         //converts the response object which container info about response(like code,headers,etc) to text
     }catch(err){                                //for handling errors in url
         console.log(`error in fetch: ${err.message}, on page ${currentUrl}`);
-    }                           
+    }
+    
+    const nextUrls = getUrlsFromHttp(htmlBody, baseUrl);    //for nested URLs inside the html page
+    for(const nextUrl of nextUrls){
+        pages = await crawlPage(baseUrl,nextUrl,pages);
+    }
+    return pages;
 }
 
 function getUrlsFromHttp(htmlBody, baseUrl){
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {crawlPage} = require('./crawl.js')
 
-function main(){
+async function main(){
     if(process.argv.length<3){
         console.log("No website provided");
         process.exit(1);
@@ -12,7 +12,10 @@ function main(){
  
     const baseUrl = process.argv[2];
     console.log(`Starting crawler on URL ${baseUrl}`);
-    crawlPage(baseUrl);
+    const pages = await crawlPage(baseUrl, baseUrl, {});
+    for(const page of Object.entries(pages)){
+        console.log(page);
+    }
 }
 
 main()
