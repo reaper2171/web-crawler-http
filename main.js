ad2bd081581a676b0e8ba21465beec252bee70f9
reaper2171 <theprodigalson2171@gmail.com>
2023-12-02 13:47:27 +0530

    added sortPage and reporting

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {crawlPage} = require('./crawl.js')
+const {printReport} = require('./report.js')
 
 async function main(){
     if(process.argv.length<3){
@@ -13,9 +14,8 @@ async function main(){
     const baseUrl = process.argv[2];
     console.log(`Starting crawler on URL ${baseUrl}`);
     const pages = await crawlPage(baseUrl, baseUrl, {});
-    for(const page of Object.entries(pages)){
-        console.log(page);
-    }
+
+    printReport(pages);
 }
 
 main()
